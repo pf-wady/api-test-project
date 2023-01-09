a8339d2add446b5da31af8332ce12176f03efb2b
pf-wady <pf.wady@gmail.com>
2023-01-09 15:01:14 +0900

    add function

---
+++ src/postHandler.ts
@@ -0,0 +1,51 @@
+import type { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { TimestreamWriteClient, WriteRecordsCommand } from "@aws-sdk/client-timestream-write";
+
+export const hello = async (event: APIGatewayEvent, context:Context): Promise<APIGatewayProxyResult> => {
+    
+    const currentTime = Date.now().toString();
+
+    const writeClient = new TimestreamWriteClient({region: "ap-northeast-1"});
+    const request = new WriteRecordsCommand({
+        DatabaseName: "LambdaTestDB",
+        TableName: "testTable",
+        Records: [
+            {
+                Dimensions: [
+                    {
+                        Name: "region",
+                        Value: "ap-northeast-1"
+                    }
+                ],
+                MeasureName: "testMetrics",
+                MeasureValueType: "MULTI",
+                MeasureValues: [
+                    {
+                        Name: "Temperature",
+                        Value: "20.0",
+                        Type: "DOUBLE"
+                    },
+                    {
+                        Name: "Pressure",
+                        Value: "1014.0",
+                        Type: "DOUBLE"
+                    }
+                ],
+                Time: currentTime,
+            }
+        ]
+    });
+    const response = await writeClient.send(request);
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+                response: response,
+            },
+            null,
+            2
+        )
+    }
+};
